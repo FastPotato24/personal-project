cbdc56b1fd8d99899c34d41f8ac564bfcdeb4813
FastPotato24 <152335363+FastPotato24@users.noreply.github.com>
2024-06-01 22:05:46 +0000

    Added base of 'Item' class and some file paths
    Updated 'main.py' to initialize some modules and make a barebones 'Item' class.
    Also preemptively set up the file paths.

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
+import json
+import os
 
+from rich import print
+from rich.console import Console
+
+console = Console()
+
+id_cache = {
+  "entities": {},
+  "items": {},
+}
+
+class Item:
+  def __init__(self, id=None, data=None):
+    if data is not None:
+      self.data = data
+    if id is not None:
+      self.id = id
+      self.load(id)
+    self.name = self.data["info"]["name"]
+
+  def load(self, id):
+    if ":" not in id:
+      self.id = "main:" + id
+    if self.id in id_cache["items"]:
+      self.data = id_cache["items"][self.id]
+      return
+    datapack, id = self.id.split(":")
+    with open(f"datapacks/{datapack}/items/{id}.json") as file: # Change file to something more readable
+      self.data = json.load(file)
