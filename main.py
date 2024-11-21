532ac7173efc99eace10ff898e567d77f939577e
FastPotato24 <152335363+FastPotato24@users.noreply.github.com>
2024-11-21 22:30:14 +0000

    Made the board prototype + card class

--- main.py
+++ main.py
@@ -1,31 +1,77 @@
-import json
-import os
-
+import rich
 from rich import print
-from rich.console import Console
+from rich.layout import Layout
+from rich.panel import Panel
+import numpy as np
 
-console = Console()
 
-id_cache = {
-  "entities": {},
-  "items": {},
-}
+class Board:
+    def __init__(self, size: tuple[int, int]):
+        self.grid = np.asarray([[None for _ in range(size[0])]
+                               for _ in range(size[1])])
 
-class Item:
-  def __init__(self, id=None, data=None):
-    if data is not None:
-      self.data = data
-    if id is not None:
-      self.id = id
-      self.load(id)
-    self.name = self.data["info"]["name"]
+        # Initiate Card Layout
+        self.layout = Layout(name="Window")
+        self.layout.split_column(
+            Layout(name="Spacer", size=2),
+            Layout(name="Board"),
+            Layout(name="UI")
+        )
+        self.layout["Board"].split_column(
+            Layout(name="y0"),
+            Layout(name="y1")
+        )
+        for y in range(len(self.grid)):
+            if y >= 2:
+                self.layout["Board"].add_split(
+                    Layout(name=f"y{y}")
+                )
+            for x in range(len(self.grid[y])):
+                if x == 0:
+                    self.layout[f"y{y}"].split_row(
+                        Layout(name=f"(0,{y})"),
+                        Layout(name=f"(1,{y})")
+                    )
+                elif x >= 2:
+                    self.layout[f"y{y}"].add_split(
+                        Layout(name=f"({x},{y})")
+                    )
 
-  def load(self, id):
-    if ":" not in id:
-      self.id = "main:" + id
-    if self.id in id_cache["items"]:
-      self.data = id_cache["items"][self.id]
-      return
-    datapack, id = self.id.split(":")
-    with open(f"datapacks/{datapack}/items/{id}.json") as file: # Change file to something more readable
-      self.data = json.load(file)
+    def render(self):
+        for y, column in enumerate(self.grid):
+            for x, card in enumerate(column):
+                self.layout[f"({x},{y})"].update(
+                    renderable=card.panel(
+                        self) if card is not None else Panel(" ")
+                )
+        print(self.layout)
+
+
+class Card:
+    def __init__(self, name: str, stats: tuple[int | None, int | None]):
+        self.name = name
+        self.power = stats[0]
+        self.health = stats[1]
+
+    def panel(self, board) -> Panel:
+        console = rich.get_console()
+        width = console.width
+        space = (width - 6*len(board.grid[0]))//len(board.grid[0]) - 4
+        return Panel(" ", title=self.name, subtitle=f"{self.power} {'─'*space} {self.health} ")
+
+
+class Encounter:
+    def __init__(self, board: Board, totem: str | None = None):
+        self.turns = 0
+        self.currentBoard = board
+        self.totem = totem
+        self.bottomRow = self.currentBoard.grid[len(self.currentBoard.grid)-1]
+
+    def endTurn(self):
+        for card in self.bottomRow:
+            pass  # TODO: Cards act here
+
+
+test = Board((4, 3))
+test.grid[0, 1] = Card("Stoat", (1, 1))
+test.render()
